d5e05aace4952c6221d6302d39d97ea45f7a7703
ADMIN <ADMIN@HP>
2021-05-04 22:48:58 -0700

    First Commit........

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 5, num2 = 15, sum;
+	      sum = num1 + num2;
+
+	      System.out.println("Sum of these numbers: "+sum);
+	}
+
+}
